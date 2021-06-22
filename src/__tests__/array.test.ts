a954b58292dad0f91f4598c0aa526eebf4d57a82
Paweł Bednarczyk <bedpaw97@gmail.com>
2021-06-22 19:23:13 +0200

    Add arrayUtils removeItemByIndex

--- src/__tests__/array.test.ts
+++ src/__tests__/array.test.ts
@@ -1,11 +1,13 @@
 import { arrayUtils } from '../array';
 
 test('removeItem', () => {
-    const array = ['a', 'b', 'c', 'd']
-    arrayUtils.removeItem(array, 'b')
-    expect(array).toEqual(['a', 'c', 'd']);
+  const array = ['a', 'b', 'c', 'd'];
+  arrayUtils.removeItem(array, 'b');
+  expect(array).toEqual(['a', 'c', 'd']);
 });
-//
-// test('removeItemByIndex', () => {
-//     expect(Greeter('Carl')).toBe('Hello Carl');
-// });
+
+test('removeItemByIndex', () => {
+    const array = [1, 2];
+    arrayUtils.removeItemByIndex(array, 1);
+    expect(array).toEqual([1]);
+});
--- src/array.ts
+++ src/array.ts
@@ -1,9 +1,9 @@
 export const arrayUtils = {
-    removeItem <T> (array: T[], toRemove: T): T[] {
-        const indexToRemove = array.indexOf(toRemove);
-        return array.splice(indexToRemove, 1);
-    },
-    removeItemByIndex <T> (array: T[], indexToRemove: number): T[] {
-        return array.splice(indexToRemove, 1);
-    },
+  removeItem<T>(array: T[], toRemove: T): T[] {
+    const indexToRemove = array.indexOf(toRemove);
+    return array.splice(indexToRemove, 1);
+  },
+  removeItemByIndex<T>(array: T[], indexToRemove: number): T[] {
+    return array.splice(indexToRemove, 1);
+  },
 };
