ebea6485470cdac118efa753c0a9946b37b4e78e
Pawel Bednarczyk <bedpaw97@gmail.com>
2021-12-05 14:04:28 +0100

    #560 Change root http root url

--- src/models/http.ts
+++ src/models/http.ts
@@ -1,45 +1,44 @@
-type SuccessFunction = <T>(data: any) => T;
+type SuccessFunction<In, Out> = (data: In) => Out
 export type ErrorFunction<T> = (error: string, defaultOptions: T, options?: Partial<T>) => string | Error;
-type WarnFunction<T> = (warn: string,  defaultOptions: T, options?: Partial<T>,) => void;
+type WarnFunction<T> = (warn: string, defaultOptions: T, options?: Partial<T>,) => void;
 
-
-export interface HttpServiceConfig<ErrorOptions, WarnOptions> {
+export interface HttpServiceConfig<ErrorOptions, WarnOptions, In, Out> {
   apiUrl: string;
   defaultRequestConfig: RequestInit;
   earlyCatchStrategy: boolean
   connectionErrorCallback: ErrorFunction<ErrorOptions>;
   defaultErrorCallback: ErrorFunction<ErrorOptions>;
-  defaultSuccessCallback: SuccessFunction;
+  defaultSuccessCallback: SuccessFunction<In, Out>
   defaultWarnCallback: WarnFunction<WarnOptions>;
   defaultErrorConfig: ErrorOptions
   defaultWarnConfig: WarnOptions
 }
 
-export interface HttpBaseConfig<ErrorOptions, WarnOptions> {
-  url: string;
-  requestConfig?: RequestInit;
-  responseConfig?: ResponseConfig<ErrorOptions, WarnOptions>;
-}
-
-export interface HttpQueryConfig<ErrorOptions, WarnOptions>
-    extends HttpBaseConfig<ErrorOptions, WarnOptions> {
-  queryObject?: Object;
-}
-
-export interface HttpCommandConfig<ErrorOptions, WarnOptions>
-    extends HttpBaseConfig<ErrorOptions, WarnOptions> {
-  body?: unknown;
-}
-
-export interface HttpConfig<ErrorOptions, WarnOptions>
-    extends HttpCommandConfig<ErrorOptions, WarnOptions> {
-  method: 'GET' | 'POST' | 'PUT' | 'DELETE'
-}
-
-export interface ResponseConfig<ErrorOptions, WarnOptions> {
-  successCallback?: SuccessFunction
+export interface ResponseConfig<ErrorOptions, WarnOptions, In, Out> {
+  successCallback?: SuccessFunction<In, Out>
   errorCallback?: ErrorFunction<ErrorOptions>
   warnCallback?: WarnFunction<WarnOptions>
   errorConfig?: ErrorOptions
   warnConfig?: WarnOptions
+}
+
+export interface HttpBaseConfig<ErrorOptions, WarnOptions, In, Out> {
+  url: string;
+  requestConfig?: RequestInit;
+  responseConfig?: ResponseConfig<ErrorOptions, WarnOptions, In, Out>;
+}
+
+export interface HttpQueryConfig<ErrorOptions, WarnOptions, In, Out>
+    extends HttpBaseConfig<ErrorOptions, WarnOptions, In, Out> {
+  queryObject?: {[key:string]: string | number}
+}
+
+export interface HttpCommandConfig<ErrorOptions, WarnOptions, In, Out>
+    extends HttpBaseConfig<ErrorOptions, WarnOptions, In, Out> {
+  body?: unknown;
+}
+
+export interface HttpConfig<ErrorOptions, WarnOptions, In, Out>
+    extends HttpCommandConfig<ErrorOptions, WarnOptions, In, Out> {
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE'
 }
