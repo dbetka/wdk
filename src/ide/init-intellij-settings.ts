8404e9d5923db286d454b7d7f482fc8749bebd75
dbetka <kontakt@dbetka.pl>
2022-09-19 10:18:33 +0200

    Create InitIntellijSettings lib

---
+++ src/ide/init-intellij-settings.ts
@@ -0,0 +1,78 @@
+import * as chalk from 'chalk'
+import * as fs from 'fs';
+import * as path from 'path';
+import * as xml2js from 'xml2js';
+
+export interface SingleModifier {
+  name: string
+  defaultXMLPath: string
+  targetXMLPath: string
+  validator(json:string): boolean
+  modifier(json:string): string
+}
+
+export async function initIntellijSettings(modifiers:SingleModifier[]) {
+  try {
+    shell.clear();
+    shell.newLine();
+    shell.write(chalk.bold('Initialize Intellij configuration\n'));
+
+    for (const modifierConfig of modifiers) {
+      shell.newLine()
+      shell.write('  ' + modifierConfig.name)
+      await modifyXMLSettings(modifierConfig)
+    }
+
+    shell.newLine();
+    shell.newLine();
+    shell.write(chalk.bold.green('  All done!'));
+  }
+  catch (err) {
+    shell.error(err);
+    process.exit(1);
+  }
+}
+
+async function modifyXMLSettings (config:SingleModifier) {
+  const {
+    defaultXMLPath,
+    targetXMLPath,
+    validator,
+    modifier,
+  } = config
+
+  if (fs.existsSync(defaultXMLPath) === false) throw new Error(`File "${defaultXMLPath}" must exists.`);
+
+  const defaultXMLString = fs.readFileSync(defaultXMLPath, 'utf-8');
+  const targetXMLExists = fs.existsSync(targetXMLPath);
+  const dirnameTarget = path.dirname(targetXMLPath);
+
+  if (targetXMLExists === false) {
+    fs.existsSync(dirnameTarget) === false && fs.mkdirSync(dirnameTarget, { recursive: true });
+    fs.writeFileSync(targetXMLPath, defaultXMLString);
+  }
+  else {
+    const targetXML = fs.readFileSync(targetXMLPath, 'utf-8');
+
+    const targetJSON = await xml2js.parseStringPromise(targetXML);
+    if (validator(targetJSON)) throw new Error(`File "${targetXMLPath}" has invalid structure or is corrupted.`);
+
+    const modifiedTargetJSON = modifier(targetJSON);
+
+    const builder = new xml2js.Builder();
+    const newTargetXML = builder.buildObject(modifiedTargetJSON);
+
+    fs.writeFileSync(targetXMLPath, newTargetXML);
+  }
+
+  shell.write(chalk.green.bold('    done '));
+  shell.write(chalk.green(targetXMLPath));
+}
+
+const shell = {
+  error: (err:unknown) => process.stderr.write(chalk.red(err)),
+  clear: () => process.stdout.write('\x1Bc'),
+  write: (text:string) => process.stdout.write(text),
+  newLine: (count:number = 1) => process.stdout.write('\n'.repeat(count)),
+  tab: (count:number = 1) => process.stdout.write('  '.repeat(count)),
+};
