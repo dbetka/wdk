8439542a46698bf5af510d2c068d5b27cb040227
dbetka <kontakt@dbetka.pl>
2022-09-22 11:16:30 +0200

    Add logs to CLI

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
-import {exec} from "child_process";
+import {execSync} from 'child_process';
+import {shell} from './helpers/shell';
 
 if (process.argv) {
   const [interpreter, script, ...args] = process.argv;
 
-  exec('npx ' + args.join(' '));
+  shell.write('npx ' + args.join(' '));
+
+  const output = execSync('npx ' + args.join(' '));
+  shell.write(output.toString('utf-8'));
 }
